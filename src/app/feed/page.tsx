aa8592e2be51657fff146865999219bda55ca622
TinajeroOC <88462409+TinajeroOC@users.noreply.github.com>
2024-11-23 11:47:26 -0800

    Update feed page
    Match margins with settings page

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,7 +1,7 @@
 export default function FeedPage() {
   return (
-    <div className='my-4 flex w-full flex-col px-4 md:px-8'>
-      <main className='min-w-full'>
+    <div className='my-8 flex w-full flex-col px-4 md:px-8'>
+      <main className='w-full'>
         <h1>Careerly</h1>
       </main>
     </div>
